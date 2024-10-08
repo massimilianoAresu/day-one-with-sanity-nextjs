4d405bcfc672d7159b63db1424bc93e44a72842c
massimilianoAresu <massimiliano.aresu@it.ey.com>
2024-10-08 12:53:36 +0200

    Added Environment Variables

--- src/sanity/client.ts
+++ src/sanity/client.ts
@@ -1,8 +1,8 @@
 import { createClient } from "next-sanity";
 
 export const client = createClient({
-  projectId: "va9hvs58",
-  dataset: "production",
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
   apiVersion: "2024-01-01",
-  useCdn: false,
+  useCdn: process.env.NODE_ENV === 'production',
 });
